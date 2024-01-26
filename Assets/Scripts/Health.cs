fa159c15aa349faa43fafa629c0b929507343a10
Arfushs <36006828+Arfushs@users.noreply.github.com>
2024-01-27 01:19:20 +0300

    Hit efffect

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    private float MAX_HEALTH;
+    [SerializeField] private float MAX_HEALTH;
     private float current_health;
+
+    private void Awake()
+    {
+        current_health = MAX_HEALTH;
+    }
 
     public float MaxHealth
     {
